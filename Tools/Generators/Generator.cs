31ed2d36a7bb8ed7e4aa104da70316a605dc1842
Akbar Asghari <akbarasghari@outlook.com>
2022-08-19 08:36:41 +0430

    add Generator and add guid in generator

---
+++ Tools/Generators/Generator.cs
@@ -0,0 +1,18 @@
+﻿using Base.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tools.Generators
+{
+    public class Generator
+    {
+        public ToolResult GuidGenerator()
+        {
+            return new ToolResult { Message = Guid.NewGuid().ToString(), IsSuccess = true };
+        }
+
+    }
+}
